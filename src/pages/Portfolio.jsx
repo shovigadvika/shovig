402e0679a4f53040091383b0b1614fb018dfb711
shovigadvika <Dmshovig@gmail.com>
2025-07-14 22:05:57 +0530

    Update Portfolio.jsx

--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import PortfolioHero from '../components/Portfolio/PortfolioHero'
 import PortfolioList from '../components/Portfolio/PortfolioList'
 import PortfolioShowCase from '../components/Portfolio/PortfolioShowCase'
-import CreativeCTA from '../components/Portfolio/CreativeCta'
+import CreativeCTA from '../components/Portfolio/CreativeCTA'
 
 const Portfolio = () => {
   return (
